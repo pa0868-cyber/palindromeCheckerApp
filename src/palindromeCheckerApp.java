3202c4577a7757b9426fc3419aaeb7060dc51335
pa0868 <pa0868@srmist.edu.in>
2026-02-25 15:01:46 +0530

    UC1: Application Entry & Welcome Message

---
+++ src/palindromeCheckerApp.java
@@ -0,0 +1,12 @@
+public class palindromeCheckerApp {
+    public static void main(String[] args) {
+        System.out.println("==============");
+        System.out.println("palidromeCheckerApp");
+        System.out.println("===============");
+        System.out.println("version:1.0");
+        System.out.println("Developed for learning java concept");
+        System.out.println("-------------------");
+        System.out.println("Application started successfully!");
+        System.out.println("Ready to proceed to next use case");
+    }
+}
